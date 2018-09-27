f68ee65486064c0beedbbab31457169742216655
dale <dale@lifesonic.cn>
2018-09-27 16:38:00 +0800

    lambda

--- app/src/main/java/com/example/kdl/weather/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/kdl/weather/ui/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kdl.weather.ui.activity
 
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
@@ -8,26 +9,13 @@ import android.view.View
 import android.widget.ProgressBar
 import com.example.kdl.weather.R
 import com.example.kdl.weather.domain.commands.RequestForecastCommand
-import com.example.kdl.weather.domain.model.Forecast
 import com.example.kdl.weather.toast
 import com.example.kdl.weather.ui.adapter.ForecastListAdapter
 import org.jetbrains.anko.async
 import org.jetbrains.anko.find
-import org.jetbrains.anko.longToast
 import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
-
-    private val items = listOf(
-            "Mon 6/23 - Sunny - 31/17",
-            "Tue 6/24 - Foggy - 21/8",
-            "Wed 6/25 - Cloudy - 22/17",
-            "Thurs 6/26 - Rainy - 18/11",
-            "Fri 6/27 - Foggy - 21/10",
-            "Sat 6/28 - TRAPPED IN WEATHER STATION - 23/18",
-            "Sun 6/29 - Sunny - 20/7"
-    )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,15 +26,19 @@ class MainActivity : AppCompatActivity() {
         async {
             val result = RequestForecastCommand("94043").execute()
             uiThread {
-                longToast("ForecastRequest performed")
                 progressBar.visibility = View.GONE
-                forecastList.adapter = ForecastListAdapter(result,
-                        object : ForecastListAdapter.OnItemClickListener {
-                            override fun invoke(forcast: Forecast) {
-                                toast(forcast.date)
-                            }
-                        })
+                forecastList.adapter = ForecastListAdapter(result) { forecast -> toast(forecast.date) }
             }
+        }
+
+        supportsLollipop {
+
+        }
+    }
+
+    inline fun supportsLollipop(code: () -> Unit) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            code()
         }
     }
 }
--- app/src/main/java/com/example/kdl/weather/ui/adapter/ForecastListAdapter.kt
+++ app/src/main/java/com/example/kdl/weather/ui/adapter/ForecastListAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.kdl.weather.ui.adapter
 
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,12 +9,10 @@ import android.widget.TextView
 import com.example.kdl.weather.R
 import com.example.kdl.weather.domain.model.ForeCastList
 import com.example.kdl.weather.domain.model.Forecast
-import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import org.jetbrains.anko.find
-import java.lang.Exception
 
-class ForecastListAdapter(val weekForecast: ForeCastList, val itemClick: OnItemClickListener) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+class ForecastListAdapter(val weekForecast: ForeCastList, val itemClick: (Forecast) -> Unit) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_forcast, parent, false)
         return ViewHolder(view, itemClick)
@@ -27,12 +24,12 @@ class ForecastListAdapter(val weekForecast: ForeCastList, val itemClick: OnItemC
 
     override fun getItemCount(): Int = weekForecast.size()
 
-    public interface OnItemClickListener {
+//    interface OnItemClickListener {
+//
+//        operator fun invoke(forcast: Forecast)
+//    }
 
-        operator fun invoke(forcast: Forecast)
-    }
-
-    class ViewHolder(view: View, val itemClick: OnItemClickListener) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View, val itemClick: (Forecast) -> Unit) : RecyclerView.ViewHolder(view) {
         private val iconView: ImageView = view.find(R.id.icon)
         private val dateView: TextView = view.find(R.id.date)
         private val descriptionView: TextView = view.find(R.id.description)
@@ -42,20 +39,11 @@ class ForecastListAdapter(val weekForecast: ForeCastList, val itemClick: OnItemC
         fun bindForecast(forecast: Forecast) {
             with(forecast) {
                 Picasso.get().setIndicatorsEnabled(true)
-                Picasso.get().load(iconUrl).placeholder(R.mipmap.ic_launcher_round).into(iconView,
-                        object : Callback {
-                            override fun onSuccess() {
-                            }
-
-                            override fun onError(e: Exception?) {
-                                Log.e("picLoadFail", e.toString())
-                            }
-
-                        })
+                Picasso.get().load(iconUrl).placeholder(R.mipmap.ic_launcher_round).into(iconView)
                 dateView.text = date
                 descriptionView.text = description
-                maxTemperatureView.text = "${high.toString()}"
-                minTemperatureView.text = "${low.toString()}"
+                maxTemperatureView.text = "$high"
+                minTemperatureView.text = "$low"
                 itemView.setOnClickListener {
                     itemClick(forecast)
                 }
